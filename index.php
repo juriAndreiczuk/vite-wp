73b2ba2a43065cc2f17c9833f3f4d302bb2687aa
juriAndreiczuk <andreiczuk@tutanota.com>
2025-10-31 16:50:38 +0100

    resolve problem with single posts template in index.php

--- index.php
+++ index.php
@@ -2,14 +2,23 @@
 <?php
 
 $page_templates = [
-  'is_404' => '404',
-  'is_single' => 'single',
-  'is_archive' => 'archive'
+    "is_404" => "404",
+    "is_archive" => "archive"
 ];
 
 foreach ($page_templates as $condition => $template) {
-  if (function_exists($condition) && $condition()) {
-    echo $blade->run($template);
+    if (function_exists($condition) && $condition()) {
+        echo $blade->run($template);
+        exit;
+    }
+}
+
+if (is_single()) {
+    $object = get_queried_object();
+    $post_type = $object instanceof WP_Post ? $object->post_type : null;
+
+    $file_name = $post_type === "post" ? "single" : "single-{$post_type}";
+
+    echo $blade->run($file_name);
     exit;
-  }
 }
