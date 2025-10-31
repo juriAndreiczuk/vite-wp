668dbad08d3b7e3d5c11ad9632570b3eafe39ed7
juriAndreiczuk <andreiczuk@tutanota.com>
2025-10-31 16:46:51 +0100

    create get_webp function

--- inc/helpers/utils.php
+++ inc/helpers/utils.php
@@ -4,3 +4,21 @@ function images($val) {
   $target_folder = VITE_ENVIRONMENT_TYPE === 'dev' ? 'src/img/' : 'dist/assets/';
   return bloginfo('template_directory').'/'.$target_folder.$val;
 }
+
+function get_webp(string $img): ?string {
+  $uploads = wp_get_upload_dir();
+  $path = str_replace($uploads['baseurl'], $uploads['basedir'], $img);
+
+  $webp_path_1 = preg_replace('/\.(jpe?g|png)$/i', '.webp', $path);
+  $webp_path_2 = $path . '.webp';
+
+  if (file_exists($webp_path_1)) {
+    return str_replace($uploads['basedir'], $uploads['baseurl'], $webp_path_1);
+  }
+
+  if (file_exists($webp_path_2)) {
+    return str_replace($uploads['basedir'], $uploads['baseurl'], $webp_path_2);
+  }
+
+  return null;
+}
