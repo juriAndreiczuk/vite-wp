4fd90050fb916f7c7d85cc0c0577b8dc1536e961
juriAndreiczuk <andreiczuk@tutanota.com>
2025-10-31 16:44:03 +0100

    remove unused WordPress CSS and JS on frontend

--- inc/core/theme-setup.php
+++ inc/core/theme-setup.php
@@ -22,3 +22,21 @@ class ThemeSetup {
 
 add_action('after_setup_theme', array('ThemeSetup', 'after_setup'));
 add_filter('upload_mimes', array('ThemeSetup', 'mimes'));
+
+add_action('wp_enqueue_scripts', function () {
+  wp_dequeue_style('wp-block-library');
+  wp_dequeue_style('wp-block-library-theme');
+  wp_dequeue_style('global-styles');
+  wp_dequeue_style('classic-theme-styles');
+}, 100);
+
+add_action('wp_enqueue_scripts', function () {
+  wp_dequeue_script('wp-i18n');
+  wp_deregister_script('wp-i18n');
+
+  wp_dequeue_script('wp-hooks');
+  wp_deregister_script('wp-hooks');
+
+  wp_dequeue_script('wp-polyfill');
+  wp_deregister_script('wp-polyfill');
+}, 100);
