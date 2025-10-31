edcbd1724518f2b9bc68b3bc0f43d602e6fb3be0
juriAndreiczuk <andreiczuk@tutanota.com>
2025-10-31 16:39:09 +0100

    adding exceptions to script modifier

--- inc/core/script-modifier.php
+++ inc/core/script-modifier.php
@@ -1,24 +1,32 @@
 <?php
 
 class ScriptModifier {
-  public static function async_scripts($url) {
-    if (strpos($url, '#asyncload') !== false) {
-      if (is_admin()) {
-        return str_replace('#asyncload', '', $url);
-      } else {
-        return str_replace('#asyncload', '', $url) . "' defer='defer";
-      }
-    }
-    return $url;
+  public static function async_scripts($src, $handle) {
+    return str_replace('#asyncload', '', $src);
   }
 
-  public static function defer_scripts($url) {
-    if (is_user_logged_in() || false === strpos($url, '.js')) {
-      return $url;
+  public static function defer_scripts($tag, $handle, $src) {
+    if (is_admin() || is_user_logged_in()) return $tag;
+    if (stripos($src, '.js') === false) return $tag;
+
+    static $deny = [
+      'wp-i18n', 'wp-hooks', 'wp-element', 'wp-a11y',
+      'wp-polyfill', 'jquery', 'jquery-core', 'jquery-migrate'
+    ];
+    if (in_array($handle, $deny, true)) return $tag;
+
+    global $wp_scripts;
+    if (isset($wp_scripts)) {
+      $after = $wp_scripts->get_data($handle, 'after');
+      if (!empty($after)) return $tag;
     }
-    return str_replace(' src', ' defer="defer" src', $url);
+
+    if (strpos($tag, ' defer') === false) {
+      $tag = str_replace('<script ', '<script defer ', $tag);
+    }
+    return $tag;
   }
 }
 
-add_filter('clean_url', array('ScriptModifier', 'async_scripts'), 11, 1);
-add_filter('script_loader_tag', array('ScriptModifier', 'defer_scripts'), 10);
+add_filter('script_loader_src', ['ScriptModifier', 'async_scripts'], 10, 2);
+add_filter('script_loader_tag', ['ScriptModifier', 'defer_scripts'], 10, 3);
